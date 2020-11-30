b7171415620afd84b637ad918825ab3cc596bf1f
IngSantana <wilkinsantana23@gmail.com>
2020-11-30 02:04:22 -0400

    agregamos el metodo swiperTarjetas

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('helooooooooooooooooo'),
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text('Pelicuas en Cartelera'),
+          backgroundColor: Colors.indigo,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[_swiperTarjetas()],
+          ),
+        ));
+  }
+
+  Widget _swiperTarjetas() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300.0,
+      child:  Swiper(
+  itemBuilder: (BuildContext context, int index) {
+    return new Image.network(
+      "http://via.placeholder.com/288x188",
+      fit: BoxFit.fill,
+    );
+  },
+  itemCount: 10,
+  itemWidth: 300.0,
+  layout: SwiperLayout.STACK,
+)
     );
   }
 }
